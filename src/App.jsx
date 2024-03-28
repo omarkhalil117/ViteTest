8e98759244ef9eb781a7117c8bfe9fa98c7b5e64
omarkhalil117 <omarkhalil117@gmail.com>
2024-03-28 20:47:53 +0200

    add update to create new conflict 3

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div>New Text</div>
         <h1>Add Extra Text</h1>
         <div>
           Replace Update with div
